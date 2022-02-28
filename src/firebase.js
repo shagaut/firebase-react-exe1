033139056b7cec8c1d4e1b6452c2ba228b50a98b
shagaut <shagaut@users.noreply.github.com>
2022-02-28 13:09:10 -0500

    ajout config firebase dans firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // À faire
+  apiKey: "AIzaSyBMxBnBl2ZZr0MX37inoTjEKE3qTNPzOME",
+  authDomain: "fir-react-exe1.firebaseapp.com",
+  projectId: "fir-react-exe1",
+  storageBucket: "fir-react-exe1.appspot.com",
+  messagingSenderId: "169283569781",
+  appId: "1:169283569781:web:f771efa6a2675f1a4d503c",
 };
 
 // Initialize Firebase and Firestore
